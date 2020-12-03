9572dd336ab137c03e93f42e9a5a14cfcbbb7945
jihye1211 <44031523+jihye1211@users.noreply.github.com>
2020-12-03 19:22:09 +0900

    표준 컨트롤
    표준 컨트롤(Standard Control) : 일반적으로 많이 사용되는 서버 컨트롤을 그룹으로 묶은 것

---
+++ DevStandartControl/DevStandartControl/FrmButton.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+namespace DevStandardControl
+{
+    public partial class FrmButton : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+
+        {
+            // 페이지가 로드 될 때 
+            if (!Page.IsPostBack) // Page에 IsPostBack(다시 게시)이 true가 아니면 => 처음 로드
+            {
+                txtNum.Text = "0"; //처음 로드할 때만 0으로 초기화
+            }
+        }
+        protected void btnUp_Click(object sender, EventArgs e) // 클릭하면 증가가 되도록 역할을 함
+        {
+            txtNum.Text = Convert.ToString(Convert.ToInt32(txtNum.Text) + 1); // 문자형을 숫자형으로 변환해줌
+            // convert 객체에 ToInt32를 이용함. (Convert.ToInt32(txtNum.Text)+1)에서 숫자형으로 변환한 후 
+            // Convert.ToString을 통해 다시  문자열로 변환
+        }
+        protected void btnDown_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = Convert.ToString(int.Parse(txtNum.Text) - 1);
+        }
+    }
+}
---
+++ DevStandartControl/DevStandartControl/FrmLinkButton.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+namespace DevStandardControl
+{
+    public partial class FrmLinkButton : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Empty
+        }
+        protected void lnkDotNetKorea_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("http://www.dotnetkorea.com/"); //닷네코리아 이동
+        }
+    }
+}
